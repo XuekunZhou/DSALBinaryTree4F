73c5db5588210fa2806a85e11fb744a5dcc41840
hhs-20170173 <99089409+XuekunZhou@users.noreply.github.com>
2022-02-15 15:29:31 +0100

    Remove works

--- PogingDrie/Tree.cs
+++ PogingDrie/Tree.cs
@@ -9,6 +9,11 @@ public class Tree
         Value = value;
     }
 
+    public void Get()
+    {
+
+    }
+
     public Response Find(int value)
     {
         Console.WriteLine("Find() has been called");
@@ -26,13 +31,14 @@ public class Tree
             }
         }
 
-        else if (Right != null)
+        if (Right != null)
         {
             return Right.Find(value);
         }
 
         return new Response("Not found", null);
     }
+
 
     public Response Add(int value)
     {
@@ -61,6 +67,158 @@ public class Tree
 
         Right = new Tree(value);
         return new Response("Added", Right);
+    }
+
+
+    public Response Remove(int value)
+    {
+        Console.WriteLine("Remove() has been called");
+        if (value == Value)
+        {
+            return new Response("Cant remove root node", this);
+        }
+        if (value < Value)
+        {
+            if (Left != null)
+            {
+                return Left.Remove(value, this, true);
+            }
+        }
+        if (Right != null)
+        {
+            return Right.Remove(value, this, false);
+        }
+
+        return new Response("Not found", this);
+    }
+
+    
+    private Response Remove(int value, Tree parent, bool isLeft)
+    {
+        Console.WriteLine("Remove() has been called");
+        if (value == Value)
+        {
+            switch (CountChildren())
+            {
+                case 0:
+                    return RemoveNoChildren(parent, isLeft);
+
+                case 1:
+                    return RemoveOneChild(parent, isLeft);
+
+                case 2:
+                    return RemoveTwoChildren(parent, isLeft);
+            }
+
+        }
+        if (value < Value)
+        {
+            if (Left != null)
+            {
+                return Left.Remove(value, this, true);
+            }
+        }
+        if (Right != null)
+        {
+            return Right.Remove(value, this, false);
+        }
+
+        return new Response("Not found", this);
+    }
+
+    private Response RemoveNoChildren(Tree parent, bool isLeft)
+    {
+        if (isLeft)
+        {
+            parent.Left = null;
+        }
+        else
+        {
+            parent.Right = null;
+        }
+
+        return new Response("Removed", parent);
+    }
+
+    private Response RemoveOneChild(Tree parent, bool isLeft)
+    {
+        if (this.Left != null)
+        {
+            if (isLeft)
+            {
+                parent.Left = this.Left;
+            }
+            else
+            {
+                parent.Right = this.Left;
+            }
+        }
+        else
+        {
+            if (isLeft)
+            {
+                parent.Left = this.Right;
+            }
+            else
+            {
+                parent.Right = this.Right;
+            }
+        }
+
+        return new Response("Removed", parent);
+    }
+
+    private Response RemoveTwoChildren(Tree parent, bool isLeft)
+    {
+        var min = Right.Min(this)[0];
+        var minParent = Right.Min(this)[1];
+
+        min.Left = this.Left;
+
+        if (min.Value != this.Right.Value)
+        {
+            min.Right = this.Right;
+        }
+        minParent.Left = null;
+
+        if (isLeft)
+        {
+            parent.Left = min;
+        }
+        else 
+        {
+            parent.Right = min;
+        }
+
+        return new Response("Removed", parent);
+    }
+
+
+    private int CountChildren()
+    {
+        int count = 0;
+
+        if (Left != null)
+        {
+            count++;
+        }
+        if (Right != null)
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    private Tree[] Min(Tree parent)
+    {
+        if (Left != null)
+        {
+            return Left.Min(this);
+        }
+
+        Tree[] package = {this, parent};
+        return package;
     }
 
 }
--- test/UnitTest1.cs
+++ test/UnitTest1.cs
@@ -5,8 +5,121 @@ namespace test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void TestFind()
     {
+        // Given
+        var sut = Program.GenerateTree();
+    
+        // When
+        var result17 = sut.Find(17);
+        var result16 = sut.Find(16);
+        var result9 = sut.Find(9);
+        var result32 = sut.Find(32);
+        var result27 = sut.Find(27);
+        var result35= sut.Find(35);
+        var result14= sut.Find(14);
+    
+        // Then
+        Assert.Equal("Found", result17.Message);
+        Assert.Equal("Not found", result16.Message);
+        Assert.Equal("Found", result9.Message);
+        Assert.Equal("Not found", result32.Message);
+        Assert.Equal("Found", result27.Message);
+        Assert.Equal("Found", result35.Message);
+        Assert.Equal("Not found", result14.Message);
+    }
 
+    [Fact]
+    public void TestAdd()
+    {
+        // Given
+        var sut = Program.GenerateTree();
+
+        // When
+        var result14 = sut.Add(14);
+        var result27 = sut.Add(27);
+        var result47 = sut.Add(47);
+        var result9 = sut.Add(9);
+        var find14 = sut.Find(14);
+        var find27 = sut.Find(27);
+        var find47 = sut.Find(47);
+        var find9 = sut.Find(9);
+        var find33 = sut.Find(33);
+        
+        // Then
+        Assert.Equal("Added", result14.Message);
+        Assert.Equal("Value already in tree", result27.Message);
+        Assert.Equal("Added", result47.Message);
+        Assert.Equal("Value already in tree", result9.Message);
+        Assert.Equal("Found", find14.Message);
+        Assert.Equal("Found", find27.Message);
+        Assert.Equal("Found", find47.Message);
+        Assert.Equal("Found", find9.Message);
+        Assert.Equal("Not found", find33.Message);
+    }
+
+    [Fact]
+    public void RemoveNoChildren()
+    {
+        // Given
+        var sut = Program.GenerateTree();
+    
+        // When
+        var result1 = sut.Remove(1);
+        var result21 = sut.Remove(21);
+        var result14 = sut.Remove(14);
+        var find1 = sut.Find(1);
+        var find21 = sut.Find(21);
+    
+        // Then
+        Assert.Equal("Removed", result1.Message);
+        Assert.Equal("Removed", result21.Message);
+        Assert.Equal("Not found", result14.Message);
+        Assert.Equal("Not found", find1.Message);
+        Assert.Equal("Not found", find21.Message);
+    }
+
+    [Fact]
+    public void RemoveOneChild()
+    {
+        // Given
+        var sut = Program.GenerateTree();
+    
+        // When
+        var result27 = sut.Remove(27);
+        var find27 = sut.Find(27);
+        var find21 = sut.Find(21);
+    
+        // Then
+        Assert.Equal("Removed", result27.Message);
+        Assert.Equal("Not found", find27.Message);
+        Assert.Equal("Found", find21.Message);
+    }
+
+    [Fact]
+    public void RemoveTwoChildren()
+    {
+        // Given
+        var sut = Program.GenerateTree();
+    
+        // When
+        var result30 = sut.Remove(30);
+        var find30 = sut.Find(3);
+        var find27 = sut.Find(27);
+        var find21 = sut.Find(21);
+        var find42 = sut.Find(42);
+        var find35 = sut.Find(35);
+        var find50 = sut.Find(50);
+    
+    
+    
+        // Then
+        Assert.Equal("Removed", result30.Message);
+        Assert.Equal("Not found", find30.Message);
+        Assert.Equal("Found", find27.Message);
+        Assert.Equal("Found", find21.Message);
+        Assert.Equal("Found", find42.Message);
+        Assert.Equal("Found", find35.Message);
+        Assert.Equal("Found", find50.Message);
     }
 }
