3c76cbd991bb10921e6ba32222ba7a5ed1c471ab
Xuekun Zhou <xuekun@live.nl>
2022-02-15 21:55:13 +0100

    Made all clases accept genereic types

--- PogingDrie/TreeNode.cs
+++ PogingDrie/TreeNode.cs
@@ -1,10 +1,10 @@
-public class TreeNode
+public class TreeNode<T> where T : IComparable
 {
-    public int Value { get; set; }
-    public TreeNode? Left { get; set; }      
-    public TreeNode? Right { get; set; }
+    public T Value { get; set; }
+    public TreeNode<T>? Left { get; set; }      
+    public TreeNode<T>? Right { get; set; }
 
-    public TreeNode(int value)
+    public TreeNode(T value)
     {
         Value = value;
     }
@@ -18,16 +18,16 @@ public class TreeNode
     // Depending on if its bigger or smaller it recursively calls the function again on either right or left
     // When it finds the value it returns a response with a message and the node holding the value
     // When it reaches a deadend or null means that the value is not in the tree and so returns a message and null
-    public Response Find(int value)
+    public Response<T> Find(T value)
     {
         Console.WriteLine("Find() has been called");
 
-        if (Value == value)
+        if (Value.CompareTo(value) == 0)
         {
-            return new Response("Found", this);
+            return new Response<T>("Found", this);
         }
         
-        if (value < Value)
+        if (Value.CompareTo(value) > 0)
         {
             if (Left != null)
             {
@@ -40,7 +40,7 @@ public class TreeNode
             return Right.Find(value);
         }
 
-        return new Response("Not found", null);
+        return new Response<T>("Not found", null);
     }
 
 
@@ -48,24 +48,24 @@ public class TreeNode
     // If the value is smaller it needs to go left and if its bigger it goes right
     // It keeps looking until it finds a null, a place to put the value. 
     // If the value already exist or it has been placed it sends a message and the node with the location
-    public Response Add(int value)
+    public Response<T> Add(T value)
     {
         Console.WriteLine("Add() has been called");
 
-        if (value == Value)
+        if (Value.CompareTo(value) == 0)
         {
-            return new Response("Value already in tree", this);
+            return new Response<T>("Value already in tree", this);
         }
 
-        if (value < Value)
+        if (Value.CompareTo(value) > 0)
         {
             if (Left != null)
             {
                 return Left.Add(value);
             }
 
-            Left = new TreeNode(value);
-            return new Response("Added", Left);
+            Left = new TreeNode<T>(value);
+            return new Response<T>("Added", Left);
         }
 
         if (Right != null)
@@ -73,20 +73,20 @@ public class TreeNode
             return Right.Add(value);
         }
 
-        Right = new TreeNode(value);
-        return new Response("Added", Right);
+        Right = new TreeNode<T>(value);
+        return new Response<T>("Added", Right);
     }
 
 
     // Depricated?
-    public Response Remove(int value)
+    public Response<T> Remove(T value)
     {
         Console.WriteLine("Remove() has been called");
-        if (value == Value)
+        if (Value.CompareTo(value) == 0)
         {
-            return new Response("Cant remove root node", null);
+            return new Response<T>("Cant remove root node", null);
         }
-        if (value < Value)
+        if (Value.CompareTo(value) > 0)
         {
             if (Left != null)
             {
@@ -98,7 +98,7 @@ public class TreeNode
             return Right.Remove(value, this, false);
         }
 
-        return new Response("Not found", null);
+        return new Response<T>("Not found", null);
     }
 
     
@@ -106,10 +106,10 @@ public class TreeNode
     // When the value has been found it checks how many children this node has
     // Depending on that it calls a function that removes the node
     // Returns a message and null
-    public Response Remove(int value, TreeNode parent, bool isLeft)
+    public Response<T> Remove(T value, TreeNode<T> parent, bool isLeft)
     {
         Console.WriteLine("Remove() has been called");
-        if (value == Value)
+        if (Value.CompareTo(value) == 0)
         {
             switch (CountChildren())
             {
@@ -124,7 +124,7 @@ public class TreeNode
             }
 
         }
-        if (value < Value)
+        if (Value.CompareTo(value) > 0)
         {
             if (Left != null)
             {
@@ -136,12 +136,12 @@ public class TreeNode
             return Right.Remove(value, this, false);
         }
 
-        return new Response("Not found", this);
+        return new Response<T>("Not found", this);
     }
 
     // If the node has no children it will remove the node by using the parent node reference to itself
     // Returns a message and null
-    private Response RemoveNoChildren(TreeNode parent, bool isLeft)
+    private Response<T> RemoveNoChildren(TreeNode<T> parent, bool isLeft)
     {
         if (isLeft)
         {
@@ -152,12 +152,12 @@ public class TreeNode
             parent.Right = null;
         }
 
-        return new Response("Removed", parent);
+        return new Response<T>("Removed", parent);
     }
 
     // If the node has a single child it will check which direction it child goes and changes the reference in its parent node to its child node
     // Returns a message and null
-    private Response RemoveOneChild(TreeNode parent, bool isLeft)
+    private Response<T> RemoveOneChild(TreeNode<T> parent, bool isLeft)
     {
         if (this.Left != null)
         {
@@ -182,13 +182,13 @@ public class TreeNode
             }
         }
 
-        return new Response("Removed", parent);
+        return new Response<T>("Removed", parent);
     }
 
     // If the node has two children it will grab the lowest value node of its right branch to be inserted in the tree on its own place.
     // The left and right branches will be added to this new root node and the reference to this node needs to be removed from its old parent
     // Returns a message and null 
-    private Response RemoveTwoChildren(TreeNode parent, bool isLeft)
+    private Response<T> RemoveTwoChildren(TreeNode<T> parent, bool isLeft)
     {
         if (Right != null && Left != null)
         {
@@ -197,7 +197,7 @@ public class TreeNode
 
             newRoot.Left = this.Left;
 
-            if (newRoot.Value != this.Right.Value)
+            if (newRoot.Value.CompareTo(this.Right.Value) != 0)
             {
                 newRoot.Right = this.Right;
             }
@@ -213,7 +213,7 @@ public class TreeNode
             }
         }
 
-        return new Response("Removed", null);
+        return new Response<T>("Removed", null);
     }
 
 
@@ -237,7 +237,7 @@ public class TreeNode
 
     // Finds the lowest value by going left until it hits a null
     // Returns a TreeNode
-    public TreeNode Min()
+    public TreeNode<T> Min()
     {
         if (Left != null)
         {
@@ -249,14 +249,14 @@ public class TreeNode
 
     // It works the same as the other Min method but for one application I also need the parent node
     // Returns an array with TreeNodes
-    public TreeNode[] Min(TreeNode parent)
+    public TreeNode<T>[] Min(TreeNode<T> parent)
     {
         if (Left != null)
         {
             return Left.Min(this);
         }
 
-        TreeNode[] package = {this, parent};
+        TreeNode<T>[] package = {this, parent};
         return package;
     }
 
